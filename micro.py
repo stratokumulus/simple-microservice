690937d7d9c0ec2e09bf9191a131e6a08cc82d2c
Marc Boss <marc.depreter@gmail.com>
2022-05-21 09:22:44 +0200

    tiny changes (can't remember, it's the next day)

--- micro.py
+++ micro.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 try:
     db_host = os.environ['DB_HOSTNAME']
     db_port = os.environ['DB_PORT']
+    useless_secret = os.environ['DB_PASSWD"]
 except:
     print ('Using default Redis configuration')
     db_host = 'redis-svc'
     db_port = 6379
+    useless_secret = 'thisIsNotUsedButIWantedToAddSecretsToMyKubernetesExample!'
 
 db = redis.Redis(host=db_host, port=db_port)
 
